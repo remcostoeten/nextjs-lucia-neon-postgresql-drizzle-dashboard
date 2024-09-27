d5d89625073f50197075080212f7a139ae2517fc
Remco Stoeten <remcostoeten@hotmail.com>
2024-09-27 18:06:15 +0200

    rename dropdown meun

---
+++ src/components/elements/dropdown-navigation/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Logo from "@/components/base/logo";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { Tabs } from "./Tabs";
+
+const fadeInVariants = {
+  hidden: { opacity: 0, y: -10 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.01 } },
+};
+
+export const DropdownNavigation = () => {
+  const pathname = usePathname();
+  if (pathname.includes("/dashboard")) return null;
+
+  return (
+    <motion.div
+      className="flex items-center w-full justify-start md:justify-center z-50"
+      initial="hidden"
+      animate="visible"
+      variants={fadeInVariants}
+    >
+      <span className="fixed z-50 top-6 flex items-center">
+        <Logo isLink={true} />
+        <Tabs />
+      </span>
+    </motion.div>
+  );
+};
