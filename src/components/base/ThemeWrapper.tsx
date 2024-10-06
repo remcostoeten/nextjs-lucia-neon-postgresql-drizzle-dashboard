9bd75b3af21be2c13569f4a79e126b989f3e8dfe
Remco Stoeten <remcostoeten@hotmail.com>
2024-10-07 00:00:09 +0200

    feat: add vercel anal-ytics

--- src/components/base/ThemeWrapper.tsx
+++ src/components/base/ThemeWrapper.tsx
@@ -1,7 +1,9 @@
 import { geistMono, geistSans } from '@/core/config/fonts/fonts'
 import { metadata } from '@/core/config/metadata/metadata.root-layout'
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Toaster, TooltipProvider } from 'ui'
+
 export { metadata }
 export default function ThemeWrapper({
 	children
@@ -25,6 +27,7 @@ export default function ThemeWrapper({
 						{children}
 					</>
 				</TooltipProvider>
+				<Analytics />
 				<SpeedInsights />
 			</body>
 		</html>
