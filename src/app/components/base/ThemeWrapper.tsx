e2daff792c785760740d663e57f40b4df584fd24
Remco Stoeten <remcostoeten@hotmail.com>
2024-10-18 00:29:45 +0200

    feat: implement posthog analytics

--- src/app/components/base/ThemeWrapper.tsx
+++ src/app/components/base/ThemeWrapper.tsx
@@ -4,10 +4,19 @@ import { metadata } from '@/core/config/metadata/metadata.root-layout'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import NextTopLoader from 'nextjs-toploader'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 import { Toaster, TooltipProvider } from 'ui'
 import { ThemeProvider } from '../ThemeProvider'
 
 export { metadata }
+
+if (typeof window !== 'undefined') {
+	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+		person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+	})
+}
 
 export default function ThemeWrapper({
 	children
@@ -15,14 +24,13 @@ export default function ThemeWrapper({
 	children: React.ReactNode
 }>) {
 	return (
-		<>
+		<PostHogProvider client={posthog}>
 			<ThemeProvider
 				attribute="class"
 				defaultTheme="system"
 				enableSystem
 				disableTransitionOnChange
 			>
-				{' '}
 				<TooltipProvider>
 					<NextTopLoader
 						color="#89777d"
@@ -40,6 +48,6 @@ export default function ThemeWrapper({
 				<Analytics />
 				<SpeedInsights />
 			</ThemeProvider>
-		</>
+		</PostHogProvider>
 	)
 }
